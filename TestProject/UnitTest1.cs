d9458976c15994850b84e8aa0430c184f6e615ec
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-09-05 22:56:41 +0200

    add-inMemory-test

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -15,34 +15,8 @@ namespace TestProject
 
             // Assert
             Assert.IsTrue(orderItem.IsActive);
-            Assert.IsTrue((DateTime.Now - orderItem.InsertDate).TotalSeconds < 1); // Comprobando que InsertDate es muy cercano a DateTime.Now
+            Assert.IsTrue((DateTime.Now - orderItem.InsertDate).TotalSeconds < 1); 
         }
 
-        [TestMethod]
-        public void Properties_ShouldGetAndSetValues()
-        {
-            // Arrange
-            var orderItem = new OrderItem
-            {
-                Id = 1,
-                IdUser = 2,
-                Username = "testuser",
-                IdIngredient = 3,
-                IngredientName = "testIngredient",
-                Amount = 10.5m,
-                Unit = "kg",
-                IsActive = false
-            };
-
-            // Assert
-            Assert.AreEqual(1, orderItem.Id);
-            Assert.AreEqual(2, orderItem.IdUser);
-            Assert.AreEqual("testuser", orderItem.Username);
-            Assert.AreEqual(3, orderItem.IdIngredient);
-            Assert.AreEqual("testIngredient", orderItem.IngredientName);
-            Assert.AreEqual(10.5m, orderItem.Amount);
-            Assert.AreEqual("kg", orderItem.Unit);
-            Assert.IsFalse(orderItem.IsActive);
-        }
     }
 }
